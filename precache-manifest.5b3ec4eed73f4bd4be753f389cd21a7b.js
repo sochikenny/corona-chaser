ddbcb750f8bb971d50e56834bdcd10f175f5d80c
Ikenna Nwajagu <Ikenna.nwajagu@gmail.com>
2020-08-04 15:39:52 -0400

    Updates

---
+++ precache-manifest.5b3ec4eed73f4bd4be753f389cd21a7b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c65d33691adea6f6c61faad77f2d275d",
+    "url": "/corona-chaser/index.html"
+  },
+  {
+    "revision": "6214f038a4636e7eb7a6",
+    "url": "/corona-chaser/static/css/main.dbc76607.chunk.css"
+  },
+  {
+    "revision": "ea870d9c97b69febeb5e",
+    "url": "/corona-chaser/static/js/2.a79c3785.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/corona-chaser/static/js/2.a79c3785.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6214f038a4636e7eb7a6",
+    "url": "/corona-chaser/static/js/main.644f2a2a.chunk.js"
+  },
+  {
+    "revision": "aa7f5489ca728bc0fc9e",
+    "url": "/corona-chaser/static/js/runtime-main.e4d1a996.js"
+  },
+  {
+    "revision": "afa5e97b5bff0f5cacf45bc625897a51",
+    "url": "/corona-chaser/static/media/Covid-img.afa5e97b.png"
+  }
+]);
