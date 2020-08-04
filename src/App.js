31b55f45fcf422d75b307ee3b430f551abd8a70c
Ikenna Nwajagu <Ikenna.nwajagu@gmail.com>
2020-08-04 15:29:33 -0400

    added a few tweaks

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Cards, Chart, CountryPicker } from './components';
 import styles from './App.module.css';
 import { fetchData } from './api';
+import image from './image/Covid-img.png';
 
 class App extends React.Component{
     state = {
@@ -25,6 +26,7 @@ class App extends React.Component{
 
         return (
             <div className={styles.container}>
+                <img className={styles.image} src={image} alt="COVID-19"/>
                 <Cards data={data} />
                 <CountryPicker handleCountryChange={this.handleCountryChange}/>
                 <Chart data={data} country={country}/>
