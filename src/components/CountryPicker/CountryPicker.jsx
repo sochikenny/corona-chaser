0d32d2b9c172fde748edd583bdea7d737fcdc4a2
Ikenna Nwajagu <Ikenna.nwajagu@gmail.com>
2020-08-03 23:42:03 -0400

    All functional code

---
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -0,0 +1,30 @@
+import React, {useState, useEffect } from 'react';
+import { NativeSelect, FormControl } from '@material-ui/core';
+
+import styles from './CountryPicker.module.css';
+
+import { fetchCountries } from '../../api';
+
+const CountryPicker = () => {
+    const [fetchedCountries, setFetchedCountries] = useState([]);
+
+    useEffect(()=> {
+        const fetchApi = async () => {
+            setFetchedCountries(await fetchCountries());
+        }
+
+        fetchApi();
+    }, [setFetchedCountries])
+
+    console.log(fetchedCountries);
+    
+    return (
+        <FormControl className={styles.formcontrol}>
+        <NativeSelect>
+        <option value='global'>Global</option>
+        </NativeSelect>
+        </FormControl>
+    )
+}
+
+export default CountryPicker;
